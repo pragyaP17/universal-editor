995f5e1265a3788c10dffed789a634397b7c4142
pragy <pragy@adobe.com>
2025-12-03 11:41:28 +0530

    Adding footer style

--- blocks/footer/footer.js
+++ blocks/footer/footer.js
@@ -11,6 +11,10 @@ export default async function decorate(block) {
   const footerPath = footerMeta ? new URL(footerMeta, window.location).pathname : '/footer';
   const fragment = await loadFragment(footerPath);
 
+//const
+const firstColumnsWrapper = document.querySelector('.footer-wrapper .columns-wrapper');
+
+
   // decorate footer DOM
   block.textContent = '';
   const footer = document.createElement('div');
