2ebb5349bb145e0813ffdcc24623a67d4cee9a65
Sagar <sadduri@adobe.com>
2025-12-09 19:57:46 +0530

    Optimize mobile FCP: add resource hints, enable eager font loading, switch FontAwesome to font-display:swap

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -102,9 +102,9 @@ async function loadEager(doc) {
 
   try {
     /* if desktop (proxy for fast connection) or fonts already loaded, load fonts.css */
-    if (window.innerWidth >= 900 || sessionStorage.getItem('fonts-loaded')) {
+    //if (window.innerWidth >= 900 || sessionStorage.getItem('fonts-loaded')) {
       loadFonts();
-    }
+    //}
   } catch (e) {
     // do nothing
   }
